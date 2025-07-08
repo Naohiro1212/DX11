82c303d6f832adebfa4bbda447ef256a4440504b
Naohiro1212 <nao1212hiro@icloud.com>
2025-07-08 16:37:06 +0900

    プロジェクト ファイルを追加します。

---
+++ Quad.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Direct3D.h"
+#include <DirectXMath.h>
+#include "Camera.h"
+#include "Texture.h"
+
+using namespace DirectX;
+
+//コンスタントバッファー
+
+struct CONSTANT_BUFFER
+{
+	XMMATRIX	matWVP;
+};
+
+//頂点情報
+struct VERTEX
+{
+	XMVECTOR position;
+	XMVECTOR uv;
+};
+
+class Quad
+{
+public:
+	Quad();
+	~Quad();
+	HRESULT Initialize();
+	void Draw(XMMATRIX& worldMatrix);
+	void Release();
+private:
+	ID3D11Buffer* pVertexBuffer_;	//頂点バッファ
+	ID3D11Buffer* pIndexBuffer_;	//インデックスバッファ
+	ID3D11Buffer* pConstantBuffer_;	// 定数バッファ
+	Texture* pTexture_;
+};
